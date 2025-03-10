4ad43a06477fb5784ca3ddf97824910a93dac08a
ChavaphonIntarasuriyawong <chavaphon.inta@mail.kmutt.ac.th>
2025-03-10 13:57:25 +0700

    Added ShoppingList

---
+++ ShoppingList/src/App.jsx
@@ -0,0 +1,14 @@
+import { TodoWrapper } from "./Components/TodoWrapper";
+import './App.css';
+
+const App = () => {
+    return (
+        <div className="Home">
+            <div className="Container">
+                <TodoWrapper />
+            </div>
+        </div>
+    );
+}
+
+export default App;
